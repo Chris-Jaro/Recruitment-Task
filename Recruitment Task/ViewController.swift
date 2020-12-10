c196c2fa3f984d7ad48d7aff94103513ccdfe9cc
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2020-12-10 18:36:24 +0100

    Recreate the first screen of the app

--- Recruitment Task/ViewController.swift
+++ Recruitment Task/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.backgroundImage = UIImage() // Removes the line
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
+
 
