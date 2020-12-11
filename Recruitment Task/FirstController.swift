b64e34e4d535860d472f0ce991c7e3c4adeb9c34
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2020-12-11 20:05:22 +0100

    Recreate custom cell for repositories table Adjust layout of "repo author name", "repo by" and stars view on the second screen to be responsive to the image

--- Recruitment Task/FirstController.swift
+++ Recruitment Task/FirstController.swift
@@ -27,12 +27,18 @@ class FirstController: UIViewController {
 }
 //MARK: - TableView DataSource Methods
 extension FirstController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell", for: indexPath) as! RepositoryCell
+        cell.layer.cornerRadius = 15
         return cell
     }
     
@@ -43,6 +49,16 @@ extension FirstController: UITableViewDataSource{
 extension FirstController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "GoToTwo", sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.white
+        return headerView
     }
 }
 
