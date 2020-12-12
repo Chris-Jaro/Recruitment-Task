3b2d8e6c5773fe6bb7c25b24fee8b4c5c39152e8
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2020-12-12 16:03:10 +0100

    Clean up the files (MVC folders) Clean up the code

--- Recruitment Task/Controllers/FirstController.swift
+++ Recruitment Task/Controllers/FirstController.swift
@@ -16,57 +16,19 @@ class FirstController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         tableView.delegate = self
         tableView.dataSource = self
-        
         tableView.register(UINib(nibName: "RepositoryCell", bundle: nil), forCellReuseIdentifier: "RepositoryCell")
-        
         searchBar.backgroundImage = UIImage() // Removes the 1px line
-    
-        performRequest(with: queryURL)
+        
+//        DispatchQueue.main.async {
+//            self.performRequest(with: self.queryURL)
+//        }
     }
 
-    
-}
-//MARK: - TableView DataSource Methods
-extension FirstController: UITableViewDataSource{
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell", for: indexPath) as! RepositoryCell
-        cell.layer.cornerRadius = 15
-        return cell
-    }
-    
-    
-}
-
-//MARK: - TableView Delegate Methods
-extension FirstController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "GoToTwo", sender: self)
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = UIColor.white
-        return headerView
-    }
-    
     func performRequest(with urlString:String) {
         if let url = URL(string: urlString){
             
@@ -97,5 +59,46 @@ extension FirstController: UITableViewDelegate{
             task.resume()
         }
     }
+    
+}
+//MARK: - TableView DataSource Methods
+extension FirstController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell", for: indexPath) as! RepositoryCell
+        cell.layer.cornerRadius = 15
+//        cell.repoTitle.text = repositories[indexPath.section].repoName
+//        cell.starsNumber.text = "\(repositories[indexPath.section].starNumber)"
+//        cell.imageView Taking the image from the internet
+        return cell
+    }
+    
+    
+}
+
+//MARK: - TableView Delegate Methods
+extension FirstController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "GoToTwo", sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.white
+        return headerView
+    }
+    
 }
 
